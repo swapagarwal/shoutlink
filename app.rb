9f0d2729f23a0c3eaa213925cfbdc4dfea5b2834
Swapnil Agarwal <swapnilagarwal@live.com>
2015-09-16 18:50:42 +0530

    Add appropriate HTTP response status codes

--- app.rb
+++ app.rb
@@ -1,6 +1,3 @@
-require 'dm-core'
-require 'dm-timestamps'
-
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://postgres:postgres@localhost/mydb')
 
 class Link
@@ -15,18 +12,29 @@ end
 DataMapper.finalize.auto_upgrade!
 
 get '/api/view/:id' do
-  Link.get(params[:id]).original
+  link = Link.get(params[:id])
+  if link.nil?
+    halt 404
+  end
+  link.original
 end
 
 get '/api/open/:id' do
-  redirect Link.get(params[:id]).original
+  link = Link.get(params[:id])
+  if link.nil?
+    halt 404
+  end
+  redirect link.original, 301
 end
 
 get '/api/create' do
   url = params[:url]
   key = Haikunator.haikunate(0)
   link = Link.new id: key, original: url
-  link.save
-  key
+  if link.save
+    status 201
+    key
+  else
+    halt 422
+  end
 end
-
