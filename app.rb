65e237aa017c06e60f12f671545251736308d29c
Swapnil Agarwal <swapnilagarwal@live.com>
2015-09-19 20:31:14 +0530

    Add optional authorization

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ class Link
   include DataMapper::Resource
 
   property :id         , String, key: true
-  property :original   , Text, format: :url
+  property :original   , Text, format: :url, required: true
+  property :password   , BCryptHash, required: false
   property :created_at , DateTime
   property :updated_at , DateTime
 end
@@ -13,27 +14,36 @@ DataMapper.finalize.auto_upgrade!
 
 get '/api/view/:id' do
   link = Link.get(params[:id])
+  password = params[:password]
   if link.nil?
     halt 404
+  end
+  if !link.password.nil? and link.password != password
+    halt 401
   end
   link.original
 end
 
 get '/api/open/:id' do
   link = Link.get(params[:id])
+  password = params[:password]
   if link.nil?
     halt 404
+  end
+  if !link.password.nil? and link.password != password
+    halt 401
   end
   redirect link.original, 301
 end
 
 get '/api/create' do
   url = params[:url]
+  password = params[:password]
   begin
     key = Haikunator.haikunate(0)
     link = Link.get(key)
   end until link.nil?
-  link = Link.new id: key, original: url
+  link = Link.new id: key, original: url, password: password
   if link.save
     status 201
     key
